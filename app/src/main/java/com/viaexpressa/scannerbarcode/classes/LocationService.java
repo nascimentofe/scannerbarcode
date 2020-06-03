be869e8f9cb29f137d0793705f0fca340d52eaef
Felipe Nascimento <felipeleforte@outlook.com>
2020-06-03 15:12:01 -0300

    adicionado delay de 3s no metodo requestLocation para que o timeout do servidor seja retornado a partir do metodo getTimeoutLocation.

--- app/src/main/java/com/viaexpressa/scannerbarcode/classes/LocationService.java
+++ app/src/main/java/com/viaexpressa/scannerbarcode/classes/LocationService.java
@@ -1,13 +1,17 @@
 package com.viaexpressa.scannerbarcode.classes;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -34,7 +38,7 @@ public class LocationService extends Service {
         super.onCreate();
 
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        mLocationCallback = new LocationCallback(){
+        mLocationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(LocationResult locationResult) {
                 super.onLocationResult(locationResult);
@@ -64,11 +68,33 @@ public class LocationService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
-    private void requestLocation(){
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(90000);
-        mLocationRequest.setFastestInterval(50000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-        mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    private void requestLocation() {
+        HTTPService request = new HTTPService(getApplicationContext());
+        final ArrayList<Long> interval = request.getTimeoutLocation();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                LocationRequest mLocationRequest = new LocationRequest();
+                mLocationRequest.setInterval((interval != null) ? interval.get(0) : 90000);
+                mLocationRequest.setFastestInterval((interval != null) ? interval.get(1) : 90000);
+                mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+                if (ActivityCompat.checkSelfPermission(LocationService.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(LocationService.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+            }
+        },  3000);
+
+
+
     }
 }
